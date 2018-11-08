960e6f31d03dfb1061229f3a2fd68889a2d5c550
Colin <colingoodale@gmail.com>
2018-11-07 20:28:43 -0600

    need to center pdf viewer

--- js/demo.js
+++ js/demo.js
@@ -1,13 +1,3 @@
-/**
- * demo.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2017, Codrops
- * http://www.codrops.com
- */
 {
 	setTimeout(() => document.body.classList.add('render'), 60);
 	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
@@ -24,11 +14,11 @@
 	document.addEventListener('keydown', (ev) => {
 		const keyCode = ev.keyCode || ev.which;
 		let linkEl;
-		if ( keyCode === 37 ) {
-			linkEl = current > 0 ? navdemos[current-1] : navdemos[total-1];
+		if (keyCode === 37) {
+			linkEl = current > 0 ? navdemos[current - 1] : navdemos[total - 1];
 		}
-		else if ( keyCode === 39 ) {
-			linkEl = current < total-1 ? navdemos[current+1] : navdemos[0];
+		else if (keyCode === 39) {
+			linkEl = current < total - 1 ? navdemos[current + 1] : navdemos[0];
 		}
 		else {
 			return false;
